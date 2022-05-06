f4c38508363511c32c92d178ac93a5ca19a936fd
Your Name <you@example.com>
2022-05-06 12:01:38 -0400

    new try

--- heap.c
+++ heap.c
@@ -18,13 +18,34 @@ typedef struct Heap{
 
 
 void* heap_top(Heap* pq){
-    return NULL;
+  return pq->heapArray[0].data;
+}
+
+void swap(heapElem  a,heapElem  b){
+   heapElem aux;
+   aux.data=a.data;
+   aux.priority=a.priority;
+   a.data=b.data;
+   a.priority=b.priority;
+   b.data=aux.data;
+   b.priority=aux.priority;
 }
 
 
-
 void heap_push(Heap* pq, void* data, int priority){
+  int a=pq->size;
+  if(pq->size == pq->capac){
+    pq->heapArray=realloc(pq->heapArray,2*(pq->capac)+1);
+  }
+  pq->heapArray[a].data=data;
+  pq->heapArray[a].priority= priority;
+  pq->size++;
 
+  while (pq->heapArray[a].priority > pq->heapArray[(a-1)/2].priority)
+  {
+     swap(pq->heapArray[a],pq->heapArray[(a-1)/2]);
+     a=(pq->size-1)/2;
+  }
 }
 
 
