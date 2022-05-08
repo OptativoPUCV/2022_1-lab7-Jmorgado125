60262631e3e20112d654576667fa68c350b94ff8
Your Name <you@example.com>
2022-05-08 19:06:06 -0400

    new try

--- heap.c
+++ heap.c
@@ -50,14 +50,14 @@ void heap_push(Heap* pq, void* data, int priority){
   }
   pq->heapArray[pq->size].data=data;
   pq->heapArray[pq->size].priority= priority;
-  int a=pq->size+1;
-
-  while (pq->heapArray[a].priority > pq->heapArray[(pq->size-1)/2].priority)
-  {
-     swap(pq->heapArray[a],pq->heapArray[(pq->size-1)/2]);
-     a=(pq->size-1)/2;
-  }
   pq->size++;
+
+  while (pq->heapArray[pq->size].priority > pq->heapArray[(pq->size-1)/2].priority)
+  {
+     swap(pq->heapArray[pq->size],pq->heapArray[(pq->size-1)/2]);
+     pq->size=(pq->size-1)/2;
+  }
+  
 }
 
 
