934adb610f820046fb7300ee22c93faff73f6422
Your Name <you@example.com>
2022-05-08 18:57:20 -0400

    new try

--- heap.c
+++ heap.c
@@ -61,7 +61,7 @@ void heap_push(Heap* pq, void* data, int priority){
 
 void heap_pop(Heap* pq){
    prin(pq);
-   //swap(pq->heapArray[0],pq->heapArray[pq->size-1]);
+   swap(pq->heapArray[0],pq->heapArray[pq->size-1]);
    pq->heapArray[pq->size-1].data=NULL;
    pq->heapArray[pq->size-1].priority=0;
 }
