ae91267d043210618decf19ec159529bc751df79
Your Name <you@example.com>
2022-05-08 22:22:07 -0400

    new try

--- heap.c
+++ heap.c
@@ -92,7 +92,7 @@ void heap_pop(Heap* pq){
          a=(2*a+2);
       }
    }
-   while(pq->heapArray[a].priority  < pq->heapArray[(a-2)/2].priority ){
+   while(pq->heapArray[a].priority  < pq->heapArray[(2*a+2)].priority ){
       if(pq->heapArray[a].priority  < pq->heapArray[(2*a+1)].priority){
          swap(pq->heapArray[a],pq->heapArray[(2*a+1)]);
          a=(2*a+1);
